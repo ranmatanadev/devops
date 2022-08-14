0bec1b5b08155fd155a1868e178bd55faab8a067
Ran Matana <ranmatanadev@gmail.com>
2022-08-14 19:53:56 +0300

    init

---
+++ pa.py
@@ -0,0 +1,61 @@
+# How to do imports
+# from distutils.log import debug
+
+
+# is_true = False
+# a = 2
+# b = 2.5
+# str_one = "One"
+# str_three = 'Three'
+
+# Types and strings
+# if type(str_one) == str and a > 1.5:
+#     print("Hi from if scope")
+
+# Lists
+# students = ["Doron", "Yuval", "Moshe"]
+# teachers = []
+# if len(teachers):
+#     print("hey")
+
+# Range
+# for number in range(5):
+#     print(number)
+
+# inputs
+# password = "123"
+# while True:
+#     pass_from_user = input("Enter password:")
+#     if pass_from_user == password:
+#         print("Hi Ran")
+#     else:
+#         print("Wrong password")
+
+# 7 Boom
+# for number in range(22):
+#     if number % 7 == 0:
+#         print(number, "Yes")
+#     else:
+#         print(number, "No")
+
+# Functions
+# def multi_number():
+#     print(2*2)
+# multi_number()
+
+# Dict
+# def getStringOfNumber(num):
+#     return Dict[num]
+# Dict = {1: 'One', 2: 'Two', 3: 'Three'}
+# print(getStringOfNumber(3))
+
+# import file function
+# One Way
+# import functionsImport
+# print(functionsImport.getUsers())
+# Another Way
+# from functionsImport import getUsers
+# print(getUsers())
+
+# print orgs
+# print("doron", "yuval", "spair", sep='!', end="")
